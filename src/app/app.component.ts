dbcca995da0e91af4f48fc0a5957092dc64f16a8
Juan Camilo Salazar Rojas <juancamilosalazarrojas@gmail.com>
2024-03-14 18:12:55 -0500

    Se crea el header, las secciones y el footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -10,5 +10,44 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'landing-page-xavier';
+  showCountryOptions: boolean = false;
+  showCityOptions: boolean = false;
+  selectedCountry: string = '';
+  selectedCity: string = '';
+
+  countries: string[] = ['País 1', 'País 2', 'País 3'];
+  cities: string[] = ['Ciudad 1', 'Ciudad 2', 'Ciudad 3'];
+
+  toggleCountryOptions() {
+    this.showCountryOptions = !this.showCountryOptions;
+    if (!this.showCountryOptions) {
+      this.selectedCountry = '';
+    }
+  }
+
+  toggleCityOptions() {
+    this.showCityOptions = !this.showCityOptions;
+    if (!this.showCityOptions) {
+      this.selectedCity = '';
+    }
+  }
+
+  selectCountry(country: string) {
+    this.selectedCountry = country;
+    this.showCountryOptions = false;
+  }
+
+  selectCity(city: string) {
+    this.selectedCity = city;
+    this.showCityOptions = false;
+  }
+
+  @HostListener('document:click', ['$event'])
+  closeOptions(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.select-container')) {
+      this.showCountryOptions = false;
+      this.showCityOptions = false;
+    }
+  }
 }
